b0c0fbe562a2ec9efc1c5af5cedb6ccb8458b7cf
Dylan Myers <dylanmyers178@gmail.com>
2020-04-13 09:56:30 -0500

    used yoonslee repository as reference, Is not working but it is a start

---
+++ Data/app.js
@@ -0,0 +1,24 @@
+let friends = [
+    {
+        name: "Lucy",
+        scores: [2, 3, 1, 5, 4]
+    },
+    {
+        name: "Aaron",
+        scores: [4, 2, 4, 5, 3]
+    },
+    {
+        name: "Tyler",
+        scores: [4, 3, 2, 1, 5] 
+    }, 
+    {
+        name: "Emily", 
+        scores: [2, 4, 6, 1, 3]
+    }
+    {
+        name: "Taylor", 
+        scores: [4, 5, 3, 4, 5] 
+    }
+];
+
+module.exports = friends;
---
+++ routes/apiToutes.js
@@ -0,0 +1,40 @@
+const friends = require("../data/friends");
+//Typed this code out using yoonslee as a reference. It is almost word for word. 
+module.exports = function(app) {
+  // Return all friends found in friends.js as JSON
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    console.log(req.body.scores);
+
+    // Receive user details
+    const user = req.body;
+
+    // parseInt for scores
+    for(var i = 0; i < user.scores.length; i++) {
+      user.scores[i] = parseInt(user.scores[i]);
+    }
+
+   
+    let bestFriendIndex = 0;
+    let minimumDifference = 40;
+
+    for(var i = 0; i < friends.length; i++) {
+      var totalDifference = 0;
+      for(var j = 0; j < friends[i].scores.length; j++) {
+        var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+        totalDifference += difference;
+      }
+
+      if(totalDifference < minimumDifference) {
+        bestFriendIndex = i;
+        minimumDifference = totalDifference;
+      }
+    }
+
+    // after finding match, add user to friend array
+    friends.push(user);
+  });
+};
